b707658bdbabd79e33531ab8072cc7cf8d0315d2
kinginthenorthcodez <kinginthenorthcodez@gmail.com>
2022-04-14 02:54:02 +0530

    Fix linter errors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,26 @@
 /* Mobile menu */
 const menuBtn = document.querySelector('#menu-btn');
-const mobileMenu  = document.querySelector('.mobile-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 const menuItems = document.querySelectorAll('.menu-item');
-
 
 function swapIcon() {
   if (mobileMenu.classList.contains('hide-menu')) {
-    menuBtn.src ='./assets/images/mobile_menu.png';
+    menuBtn.src = './assets/images/mobile_menu.png';
   } else {
-    menuBtn.src ='./assets/images/close_mobile_menu.png';
-      menuBtn.style ='background: black';
+    menuBtn.src = './assets/images/close_mobile_menu.png';
+    menuBtn.style = 'background: black';
   }
-};
+}
 
 menuBtn.addEventListener('click', () => {
- mobileMenu.classList.toggle('hide-menu');
- mobileMenu.style.transition ='all 3s';
- swapIcon();
+  mobileMenu.classList.toggle('hide-menu');
+  mobileMenu.style.transition = 'all 3s';
+  swapIcon();
 });
 
 menuItems.forEach((menuList) => {
   menuList.addEventListener('click', () => {
     mobileMenu.classList.toggle('hide-menu');
-   swapIcon();
+    swapIcon();
   });
 });
